804ef324faa7d4cbc1752d4d7f547a3d4b8e7518
nkihbk <kenchasonakai@gmail.com>
2020-10-22 22:45:21 +0900

    rubocop -a

--- Gemfile
+++ Gemfile
@@ -1,26 +1,30 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-gem 'rails',      '6.0.3'
-gem 'puma',       '4.3.4'
-gem 'sass-rails', '5.1.0'
-gem 'webpacker',  '4.0.7'
-gem 'turbolinks', '5.2.0'
-gem 'jbuilder',   '2.9.1'
-gem 'bootsnap',   '1.4.5', require: false
+gem 'bootsnap', '1.4.5', require: false
 gem 'bootstrap', '~> 4.4.1'
+gem 'jbuilder', '2.9.1'
 gem 'jquery-rails'
+gem 'puma',       '4.3.4'
+gem 'rails',      '6.0.3'
+gem 'sass-rails', '5.1.0'
+gem 'turbolinks', '5.2.0'
+gem 'webpacker', '4.0.7'
 
 group :development, :test do
+  gem 'byebug',  '11.0.1', platforms: %i[mri mingw x64_mingw]
   gem 'sqlite3', '1.4.1'
-  gem 'byebug',  '11.0.1', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
-  gem 'web-console',           '4.0.1'
-  gem 'listen',                '3.1.5'
+  gem 'listen', '3.1.5'
+  gem 'rubocop', require: false
+  gem 'rubocop-performance', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec'
   gem 'spring',                '2.1.0'
   gem 'spring-watcher-listen', '2.0.1'
+  gem 'web-console',           '4.0.1'
 end
 
 group :test do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def create
-		responses = params[:responses].permit!.to_hash
-		@post_body = {body: post_text(responses)}
-		render :json => @post_body 
-	end
+    responses = params[:responses].permit!.to_hash
+    @post_body = { body: post_text(responses) }
+    render json: @post_body
+  end
 
   private
 
@@ -12,82 +12,82 @@ class PostsController < ApplicationController
   end
 
   def post_text(responses)
-		response = responses["0"] if responses
-		if response
-			safesearch = response["safeSearchAnnotation"]
-			faceAnnotations = response["faceAnnotations"]
-			bestGuessLabels = response["webDetection"]["bestGuessLabels"]["0"]["label"]
-			label = "ふむふむ、#{bestGuessLabels}じゃな <br>"
-		end
-		
-		faceAnnotation = faceAnnotations["0"] if faceAnnotations
-		
-		if safesearch
-			adult_decision = safesearch["adult"]
-			violence_decision = safesearch["violence"]
-			safesearch = [adult_decision, violence_decision]
+    response = responses['0'] if responses
+    if response
+      safesearch = response['safeSearchAnnotation']
+      faceAnnotations = response['faceAnnotations']
+      bestGuessLabels = response['webDetection']['bestGuessLabels']['0']['label']
+      label = "ふむふむ、#{bestGuessLabels}じゃな <br>"
+    end
 
-			if adult_decision == "VERY_LIKELY"
-				adult_level = "これはとてもエチチな画像じゃ <br>"
-			elsif adult_decision == "LIKELY"
-				adult_level = "これはエッチな画像じゃ！けしからんぞい <br>"
-			elsif adult_decision == "POSSIBLE"
-				adult_level = "これはエッチな画像の可能性があるぞいこっそり見るんじゃぞ <br>"
-			else
-				adult_level = ""
-			end
+    faceAnnotation = faceAnnotations['0'] if faceAnnotations
 
-			if violence_decision == "VERY_LIKELY"
-				violence_level = "暴力はよくないぞい <br>"
-			elsif violence_decision == "LIKELY"
-				violence_level = "暴力はよくないぞい <br>"
-			elsif violence_decision == "POSSIBLE"
-				violence_level = "これはバイオレンスな画像の可能性がポッシボウじゃ <br>"
-			else
-				violence_level = ""
-			end
-		end
+    if safesearch
+      adult_decision = safesearch['adult']
+      violence_decision = safesearch['violence']
+      safesearch = [adult_decision, violence_decision]
 
-		if faceAnnotation
-			#楽しさ
-			joyLikelihood = faceAnnotation["joyLikelihood"]
-			#悲しみ
-			sorrowLikelihood = faceAnnotation["sorrowLikelihood"]
-			#怒り
-			angerLikelihood  = faceAnnotation["angerLikelihood"]
-			#驚き
-			surpriseLikelihood  = faceAnnotation["surpriseLikelihood"]
-			#"VERY_LIKELY", "LIKELY", "POSSIBLE", "UNLIKELY", "VERY_UNLIKELY"
-			if joyLikelihood == "VERY_LIKELY"
-				joy = "とってもいい笑顔じゃ喜びボルテージがマックスじゃ <br>"
-			elsif joyLikelihood == "LIKELY"
-				joy = "うむいい笑顔じゃのやっぱり笑顔が一番じゃ <br>"
-			elsif joyLikelihood == "POSSIBLE"
-				joy = "いい笑顔じゃの楽しい雰囲気が伝わってくるわい <br>"
-			else
-				joy = ""
-			end
+      adult_level = if adult_decision == 'VERY_LIKELY'
+                      'これはとてもエチチな画像じゃ <br>'
+                    elsif adult_decision == 'LIKELY'
+                      'これはエッチな画像じゃ！けしからんぞい <br>'
+                    elsif adult_decision == 'POSSIBLE'
+                      'これはエッチな画像の可能性があるぞいこっそり見るんじゃぞ <br>'
+                    else
+                      ''
+                    end
 
-			if sorrowLikelihood == "VERY_LIKELY" || sorrowLikelihood == "LIKELY" || sorrowLikelihood == "POSSIBLE"
-				sorrow = "悲しそうな顔をしとるのぅいいことがあるといいのう <br>"
-			else 
-				sorrow = ""
-			end
+      violence_level = if violence_decision == 'VERY_LIKELY'
+                         '暴力はよくないぞい <br>'
+                       elsif violence_decision == 'LIKELY'
+                         '暴力はよくないぞい <br>'
+                       elsif violence_decision == 'POSSIBLE'
+                         'これはバイオレンスな画像の可能性がポッシボウじゃ <br>'
+                       else
+                         ''
+                       end
+    end
 
-			if angerLikelihood == "VERY_LIKELY" || angerLikelihood == "LIKELY" || angerLikelihood == "POSSIBLE"
-				anger = "怒っておるの激おこぷんぷん丸じゃ <br>"
-			else 
-				anger = ""
-			end
-			
-			if surpriseLikelihood == "VERY_LIKELY" || surpriseLikelihood == "LIKELY" || surpriseLikelihood == "POSSIBLE"
-				surprise = "びっくりしておるのう何があったんじゃ <br>"
-			else 
-				surprise = ""
-			end
-			emotion = "#{joy}#{sorrow}#{anger}#{surprise}"
-		end
+    if faceAnnotation
+      # 楽しさ
+      joyLikelihood = faceAnnotation['joyLikelihood']
+      # 悲しみ
+      sorrowLikelihood = faceAnnotation['sorrowLikelihood']
+      # 怒り
+      angerLikelihood  = faceAnnotation['angerLikelihood']
+      # 驚き
+      surpriseLikelihood = faceAnnotation['surpriseLikelihood']
+      # "VERY_LIKELY", "LIKELY", "POSSIBLE", "UNLIKELY", "VERY_UNLIKELY"
+      joy = if joyLikelihood == 'VERY_LIKELY'
+              'とってもいい笑顔じゃ喜びボルテージがマックスじゃ <br>'
+            elsif joyLikelihood == 'LIKELY'
+              'うむいい笑顔じゃのやっぱり笑顔が一番じゃ <br>'
+            elsif joyLikelihood == 'POSSIBLE'
+              'いい笑顔じゃの楽しい雰囲気が伝わってくるわい <br>'
+            else
+              ''
+            end
 
-		return post_boby = "#{label}#{adult_level}#{violence_level}#{emotion}"
+      sorrow = if sorrowLikelihood == 'VERY_LIKELY' || sorrowLikelihood == 'LIKELY' || sorrowLikelihood == 'POSSIBLE'
+                 '悲しそうな顔をしとるのぅいいことがあるといいのう <br>'
+               else
+                 ''
+               end
+
+      anger = if angerLikelihood == 'VERY_LIKELY' || angerLikelihood == 'LIKELY' || angerLikelihood == 'POSSIBLE'
+                '怒っておるの激おこぷんぷん丸じゃ <br>'
+              else
+                ''
+              end
+
+      surprise = if surpriseLikelihood == 'VERY_LIKELY' || surpriseLikelihood == 'LIKELY' || surpriseLikelihood == 'POSSIBLE'
+                   'びっくりしておるのう何があったんじゃ <br>'
+                 else
+                   ''
+                 end
+      emotion = "#{joy}#{sorrow}#{anger}#{surprise}"
+    end
+
+    post_boby = "#{label}#{adult_level}#{violence_level}#{emotion}"
   end
 end
